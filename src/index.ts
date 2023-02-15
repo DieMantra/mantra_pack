d99df9931bfe36a1eb0b842fca7bb2d55d8b02e4
Tyson <100465135+DieMantra@users.noreply.github.com>
2023-02-15 20:56:26 +1100

    init: troubleshoot workflow etc...

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,9 @@ function add(a: number, b: number): number {
 function subtract(a: number, b: number): number {
 	return a - b;
 }
+
+const t = add(1, 2);
+const u = subtract(1, 2);
+
+export type Add = typeof add;
+export type Subtract = typeof subtract;
